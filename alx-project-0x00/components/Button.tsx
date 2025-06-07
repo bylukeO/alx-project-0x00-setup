61cc7f9bb72e921de2eda1bad9fbe0a32292c7d2
Luke Oladejo <bylukeo@gmail.com>
2025-06-07 12:07:34 +0100

    edits made to landing.tsx file

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 interface ButtonProps {
   title: string;
+  styles?: React.CSSProperties;
   className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ title, className }) => (
-  <button className={`px-4 py-2 bg-blue-500 text-white font-semibold ${className}`}>
+const Button: React.FC<ButtonProps> = ({ title, styles, className }) => (
+  <button
+    className={`px-4 py-2 bg-blue-500 text-white font-semibold ${className}`}
+    style={styles}
+  >
     {title}
   </button>
 );
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,16 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button"
 
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <div className="flex gap-4 my-6">
+        <Button title="Small Rounded-sm" className="rounded-sm" />
+        <Button title="Medium Rounded-md" className="rounded-md" />
+        <Button title="Large Rounded-lg" className="rounded-lg" />
+        <Button title="Full Rounded-full" className="rounded-full" />
+      </div>
       <Card />
     </div>
   )
